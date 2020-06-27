16b36b78ee3110c6eb9a55f09c2334c8ac016ef6
geonwan <psog03081@naver.com>
2020-06-27 14:49:29 +0900

    jsx문법

--- src/App.js
+++ src/App.js
@@ -2,10 +2,34 @@ import React, {Component} from 'react';
 
 class App extends Component{
   render(){
+    const text = "당신은 어썬한가요? ";
+    const condition = true;
+    const style = {
+      backgroundColor : "gray" ,
+      border : "1px solid black" , 
+      height : Math.round(Math.random() * 300) + 50 ,
+      width : Math.round(Math.random() * 300) + 50 ,
+      WebkitTransition : "all" , 
+      MoTransition : "all"
+    }
     return(
-      <div>
+      <div className="my-div">
+        {/* 요소 밖에서는 이렇게 작성할수 있다. */}
         <h1>리엑트 안녕</h1>
-        <h2>당신은 어썬한가요? </h2>
+        <h2>{text}</h2>
+        {
+          condition ? "보여주세요" : "null"
+        }
+        <br></br>
+        {
+          condition && "보여주세요"
+        }
+        <div style={style}
+          // self-closed 태그에서만 작동하는 주석
+          // 마지막 /> 가 꼭 새줄에 있어야 합니다.
+          /* 이렇게 작성할 수도 있고요 */
+        />
+        // 여긴 주석 사용 불가능
       </div>
     );
   }
