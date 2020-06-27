bf15fac1bc62e9d773058d6b8c1a56b87371223d
geonwan <psog03081@naver.com>
2020-06-27 13:56:36 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <h1>리엑트 안녕</h1>
+        <h2>당신은 어썬한가요? </h2>
+      </div>
+    );
+  }
+}
+
+export default App;
