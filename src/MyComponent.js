12cf101b9b883294945b72332553861af87a7db9
geonwan <psog03081@naver.com>
2020-06-30 19:50:33 +0900

    state 공부

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -2,6 +2,13 @@ import React , {Component} from "react";
 import PropTypes from "prop-types"
 
 class MyComponent extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            number : 0
+        }
+    }
     static defaultProps = {
         name : "기본이름"
     }    
@@ -16,6 +23,12 @@ class MyComponent extends Component{
                 나의 새롭고 멋진 컴포넌트
                 <p>안녕하세요, 제이름은 {this.props.name} 입니다.</p>
                 <p>저는 {this.props.age} 살 입니다.</p>
+                <p>숫자 : {this.state.number}</p>
+                <button onClick={() => {
+                    this.setState({
+                        number : this.state.number + 1
+                    })
+                }}>더하기</button>
             </div>
         )
     }
